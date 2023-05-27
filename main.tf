aa3ee6e3073ac6b3c70f92dc638f25780778978b
SmithaVerity <smitha@theveritycorp.com>
2023-05-27 19:48:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,26 @@
 
+terraform {
+  # This module is now only being tested with Terraform 1.1.x. However, to make upgrading easier, we are setting 1.0.0 as the minimum version.
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+  }
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "your-access-key"
+  secret_key = "your-secret-key"
+}
+
+
 variable "username" {
   type = list(string)
   default = ["CTPUser1","CMTSUser1"]

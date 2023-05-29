c4b3b577ec15e44feaba76d611fd6fea7ab8f080
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-05-29 14:19:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,25 +13,6 @@ resource "aws_iam_user" "demo" {
   name = "${element(var.username,count.index )}"
 }
 
-resource "aws_iam_user_policy" "newemp_policy" {
-	count = length(var.username)
-	name = "new"
-	user = element(var.username,count.index)
-	policy = <<EOF
-	{
-	  "Version": "2012-10-17",
-	  "Statement": [
-		{
-		  "Effect": "Allow",
-		  "Action": [
-			"ec2:Describe*"
-		  ],
-		  "Resource": "*"
-		}
-	  ]
-	}
-	EOF
-}
 
 output "user_arn" {
   value = aws_iam_user.demo.*.arn
